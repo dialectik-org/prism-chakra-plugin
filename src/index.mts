d6a911b41b68fbfb1801e0dacf78064874d33bed
Benoit Rognier <benoit.rognier@edukera.com>
2023-05-07 08:42:43 +0200

    initial commit

---
+++ src/index.mts
@@ -0,0 +1,50 @@
+import { remarkCodeFrame } from './codeframe.mjs'
+import { IDialectikPlugin, IPluginProvider } from '@dialectik/plugin-interface';
+import { Dirent, readdirSync } from 'fs';
+import { dirname, extname, join } from 'path';
+import { refractor } from 'refractor'
+import rehypePrismPlus from 'rehype-prism-plus'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const prismPlugin : IDialectikPlugin = {
+  withMain: false,
+  withComponents: true,
+  remarkPlugins: [remarkCodeFrame],
+  rehypePlugins: [rehypePrismPlus],
+  isRequired: (content: string): boolean => {
+    // Define a regex pattern to match the "``` ... ```" pattern
+    const pattern = /```[\s\S]*?```/;
+    // Test the pattern against the input content
+    return pattern.test(content);
+  },
+  requires: []
+}
+
+const loadPluginFiles = (pluginDirectory: string) => {
+  readdirSync(pluginDirectory, { withFileTypes: true }).forEach(async (entry: Dirent) => {
+    if (entry.isFile() && extname(entry.name) === '.mjs') {
+      const grammar = join(pluginDirectory, entry.name);
+      import(grammar).then((lang) => {
+        refractor.register(lang.default);
+      });
+    }
+  });
+}
+
+export const PluginProvider : IPluginProvider = {
+  getPlugin : (arg ?: any) => {
+    // __dirname is .../node_modules/@dialectik/prism-plugin/lib
+    const modulesDir = join(__dirname, '..', '..', '..');
+    //console.log(modulesDir)
+    if (arg !== undefined && arg?.length) {
+      (arg as string[]).forEach(plugin => {
+        console.log("Prism plugin is loading '" + plugin + "' plugin")
+        loadPluginFiles(join(modulesDir, plugin))
+      })
+    }
+    return prismPlugin
+  }
+}
---
+++ src/react/components.tsx
@@ -0,0 +1,91 @@
+// @ts-ignore
+import { Card, CardBody } from '@chakra-ui/react'
+// @ts-ignore
+import clsx from 'clsx';
+// @ts-ignore
+import copy from 'copy-text-to-clipboard';
+
+// @ts-ignore
+import React from 'react';
+
+import './css/prism-night-owl.css'
+import './css/prism-one-light.css'
+import './css/prism-plus.css'
+import './css/reset-prism.css'
+
+//import './css/prism-plus.css'
+
+interface CopyButtonProps {
+  readonly code: string;
+  readonly className?: string;
+}
+
+const CopyButton = ({code, className}: CopyButtonProps) => {
+  const [isCopied, setIsCopied] = React.useState(false);
+  // @ts-ignore
+  const copyTimeout = React.useRef<number | undefined>(undefined);
+  const handleCopyCode = React.useCallback(() => {
+    copy(code);
+    setIsCopied(true);
+    copyTimeout.current = window.setTimeout(() => {
+      setIsCopied(false);
+    }, 1000);
+  }, [code]);
+
+  React.useEffect(() => () => window.clearTimeout(copyTimeout.current), []);
+  console.log('CopyButton here')
+  return (
+    <button
+      type="button"
+      aria-label={
+        isCopied
+          ? 'Copied'
+          : 'Copy code to clipboard'
+      }
+      title={'Copy'}
+      className={clsx(
+        'copyButton',
+        isCopied && 'copyButtonCopied',
+      )}
+
+      onClick={handleCopyCode}>
+      <span className={'copyButtonIcons'} aria-hidden="true">
+        { isCopied
+          ? <svg className={'copyButtonSuccessIcon'} viewBox="0 0 24 24">
+              <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+            </svg>
+          : <svg className={'copyButtonIcon'} viewBox="0 0 24 24">
+              <path d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z" />
+            </svg>
+        }
+      </span>
+    </button>
+  );
+}
+
+interface CodeProps {
+  code : string,
+  children: any,
+  lang ?: string,
+  meta ?: string,
+}
+
+export const CodeFrame = ({ code, children, lang, meta } : CodeProps) => {
+  return (
+  <Card mb='1.25rem' borderRadius='4px'>
+    <CardBody>
+      {children}
+    </CardBody>
+  </Card>)
+}
+
+export const getComponents = () => {
+  return {
+    codeframe: (props: any) => <CodeFrame
+      code={props.code}
+      children={props.children}
+      lang={props.lang}
+      meta={props.meta}
+    />
+  }
+}
